93b98d9adac2b3b573e6b9b91f0b9c70d378528c
krishnadheerajp <saisuryadheeru@gmail.com>
2021-08-07 01:14:44 +0530

    added scroll effects

--- Gpug/js/customscript.js
+++ Gpug/js/customscript.js
@@ -83,3 +83,5 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function () { // On DOM Load initiate the effect
   if (textArray.length) setTimeout(type, newTextDelay + 250);
 });
+
+AOS.init();
