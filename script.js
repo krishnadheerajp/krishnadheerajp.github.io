6ce8c8d403312634700b0df2ea912919916c1c6f
P Krishna Dheeraj <63544142+krishnadheerajp@users.noreply.github.com>
2023-10-01 21:44:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,29 @@
+let btn1 = document.getElementById("edu");
+let btn2 = document.getElementById("skll");
+let btn3 = document.getElementById("proj");
 
-
-btn1 = document.getElementById("abtm");
-btn2 = document.getElementById("edu");
-btn3 = document.getElementById("skll");
-btn4 = document.getElementById("proj");
-btn5 = document.getElementById("exp");
-btn6 = document.getElementById("cm");
 btn1.addEventListener('click',()=>{
     btn1.classList.add("active");
     btn2.classList.remove("active");
     btn3.classList.remove("active");
-    btn4.classList.remove("active");
-    btn5.classList.remove("active");
-    btn6.classList.remove("active");
 })
 
 btn2.addEventListener('click',()=>{
     btn2.classList.add("active");
     btn1.classList.remove("active");
+    btn1.classList.remove("active");
     btn3.classList.remove("active");
-    btn4.classList.remove("active");
-    btn5.classList.remove("active");
-    btn6.classList.remove("active");
 })
 
 btn3.addEventListener('click',()=>{
     btn3.classList.add("active");
     btn1.classList.remove("active");
-    btn2.classList.remove("active");
-    btn4.classList.remove("active");
-    btn5.classList.remove("active");
-    btn6.classList.remove("active");
-})
-
-btn4.addEventListener('click',()=>{
-    btn4.classList.add("active");
     btn1.classList.remove("active");
     btn2.classList.remove("active");
-    btn3.classList.remove("active");
-    btn5.classList.remove("active");
-    btn6.classList.remove("active");
 })
 
-btn5.addEventListener('click',()=>{
-    btn5.classList.add("active");
-    btn1.classList.remove("active");
-    btn2.classList.remove("active");
-    btn3.classList.remove("active");
-    btn4.classList.remove("active");
-    btn6.classList.remove("active");
-})
 
-btn6.addEventListener('click',()=>{
-    btn6.classList.add("active");
-    btn1.classList.remove("active");
-    btn2.classList.remove("active");
-    btn3.classList.remove("active");
-    btn4.classList.remove("active");
-    btn5.classList.remove("active");
-})
-
-mybutton = document.getElementById("myBtn");
+let mybutton = document.getElementById("myBtn");
 
 window.onscroll = function() {scrollFunction()};
 
@@ -78,12 +41,3 @@ function topFunction() {
 }
 
 AOS.init({duration:1500, easing:'ease'});
-
-
-// abtmefiller();
-// $('.card-text').slideToggle(5000);
-// function abtmefiller() {
-//         $('#abtme').fadeToggle(5000);
-//         abtmefiller();
-// }
-// $('.card-text').slideToggle(5000);
